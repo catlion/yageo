6aa01d421c00dcb21d1c6ccf9d9debd38c33dad4
artem <ak@ak0.org>
2013-01-13 22:32:47 +0400

    Initial commit

---
+++ Model.fs
@@ -0,0 +1,37 @@
+﻿module Yageo.model
+open System
+type Point = {
+    lat: double
+    lon: double
+} with override x.ToString() =
+    x.lon.ToString("F", System.Globalization.CultureInfo.InvariantCulture)
+    + ", "
+    + x.lat.ToString("F", System.Globalization.CultureInfo.InvariantCulture)
+
+type Address = {
+    country: string
+    city: string
+    street: string
+    premise: string
+}
+
+let (!?) = fun (x: string) -> System.Web.HttpUtility.UrlEncode x
+
+type GeoRequest(request: string, key: string, ?max_count: byte, ?skip:byte, ?area_center: Point option, ?area_size: Point option) =
+    let geocode = request
+    let key = key
+    let ll = defaultArg area_center None
+    let spn = defaultArg area_size None
+    let rspn = if area_center <> None && area_size <> None then 1uy else 0uy
+    let results = defaultArg max_count 10uy
+    let skip = defaultArg skip 0uy
+    //let lang = defaultArg lang `ru-RU`
+    with
+        override x.ToString() =
+            let loc = match rspn with
+                      | 1uy -> sprintf "&rspn=1&ll=%s&spn=%s" (ll.Value.ToString()) (spn.Value.ToString())
+                      | _ -> String.Empty
+            sprintf "geocode=%s&key=%s&results=%d&skip=%d&%s" !?geocode !?key results skip !?loc
+
+type GeoResponse() =
+    member x.
---
+++ YaMaps.fs
@@ -0,0 +1,12 @@
+﻿module Yageo.map
+open System
+open System.Net
+open Yageo.model
+open ServiceStack.Text
+
+let geocode (p: GeoRequest) = async {
+    use wc = new WebClient()
+    wc.UseDefaultCredentials <- true
+    let! res = wc.AsyncDownloadString(new Uri("http://geocode-maps.yandex.ru/1.x/?format=json&" + p.ToString()))
+    return JsonSerializer.DeserializeFromString<
+}
